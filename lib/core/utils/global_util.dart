65cd6030994f0e441d881dea9ff8a8a972794e28
Abi Mulya <abimulya.2001@gmail.com>
2025-07-02 13:48:54 +0700

    add greatingText util

--- lib/core/utils/global_util.dart
+++ lib/core/utils/global_util.dart
@@ -1,4 +1,6 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:intl/intl.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -27,5 +29,23 @@ class GlobalUtil {
     final dateToday = DateFormat.yMd().add_Hms().format(DateTime.now());
     message = "[$dateToday]: $message";
     debugPrintSynchronously(message, wrapWidth: wrapWidth);
+  }
+
+  static String greatingText(BuildContext context) {
+
+    final int hourOfDay = DateTime.now().hour;
+
+    String greating = '';
+    if (hourOfDay >= 0 && hourOfDay < 11) {
+      greating = context.tr('global_good_morning');
+    } else if (hourOfDay >= 11 && hourOfDay < 15) {
+      greating = context.tr('global_good_afternoon');
+    } else if (hourOfDay >= 15 && hourOfDay < 19) {
+      greating = context.tr('global_good_afternoon2');
+    } else if (hourOfDay >= 19 && hourOfDay <= 24) {
+      greating = context.tr('global_good_evening');
+    }
+
+    return greating;
   }
 }
